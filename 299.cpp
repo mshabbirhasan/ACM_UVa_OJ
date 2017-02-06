4cdb9583c0c861799b2f132e5bcb7779d9aefb21
shabbir005 <shabbirhasan05@gmail.com>
2017-02-06 15:21:22 -0500

    problem number 299

---
+++ 299.cpp
@@ -0,0 +1,29 @@
+#include <stdio.h>           //  Accepted
+
+int main(){
+    int m, n, array[50], counter, total_swaps, i, j, temp;
+    
+    scanf("%d", &m);
+    
+    for(counter = 0; counter < m; counter++){
+        scanf("%d", &n);
+        
+        for(i = 0; i < n; i++)
+            scanf("%d", &array[i]);
+        
+        for(i = 0, total_swaps = 0; i < n; i++){
+            for(j = 1; j < n-i; j++){
+                if(array[j-1] > array[j]){
+                    temp = array[j-1];
+                    array[j-1] = array[j];
+                    array[j] = temp;
+                    total_swaps++;
+                }
+            }
+        }
+        
+        printf("Optimal train swapping takes %d swaps.\n", total_swaps);
+    }
+    
+    return 0;
+}
